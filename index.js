3a2bca0ada1985fb9a1462a06e0ddaeb43ac2f68
Aaronkst <47111815+Aaronkst@users.noreply.github.com>
2019-09-10 17:22:01 +0630

    Update index.js

--- index.js
+++ index.js
@@ -11,15 +11,20 @@ const bot = new ViberBot({
 	avatar: "https://dl-media.viber.com/1/share/2/long/vibes/icon/image/0x0/800b/b85aacbdc579999439e781f37f8bec50b1238229df591acc3996c50e97b5800b.jpg" // It is recommended to be 720x720, and no more than 100kb.
 });
 
-app.get('/webhook',(req, res) => {
+
 // Perfect! Now here's the key part:
 bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
 	// Echo's back the message to the client. Your bot logic should sit here.
 	console.log(message);
 });
+
+const port = process.env.PORT || 3000;
+app.use("/webhook", bot.middleware());
+app.listen(port, () => {
+  console.log(`Application running on port: ${port}`);
+  bot.setWebhook(`${process.env.EXPOSE_URL}/webhook`).catch(error => {
+    console.log('Can not set webhook on following server. Is it running?');
+    console.error(error);
+    process.exit(1);
+  });
 });
-
-const http = require('http');
-const port = process.env.PORT || 8080;
-
-http.createServer(bot.middleware()).listen(port, () => bot.setWebhook('https://hyperbeast.herokuapp.com/webhook'));
