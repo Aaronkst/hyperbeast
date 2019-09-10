7da6bdf3f2dc802a362c1de75bfd87bdc82797b6
Aaronkst <47111815+Aaronkst@users.noreply.github.com>
2019-09-10 18:38:03 +0630

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const ViberBot = require('viber-bot').Bot;
 const BotEvents = require('viber-bot').Events;
 const express = require('express');
 const app = express();
+const TextMessage = require('viber-bot').Message.Text;
+const UrlMessage = require('viber-bot').Message.Url;
+const ContactMessage = require('viber-bot').Message.Contact;
+const PictureMessage = require('viber-bot').Message.Picture;
+const VideoMessage = require('viber-bot').Message.Video;
+const LocationMessage = require('viber-bot').Message.Location;
+const StickerMessage = require('viber-bot').Message.Sticker;
+const RichMediaMessage = require('viber-bot').Message.RichMedia;
+const KeyboardMessage = require('viber-bot').Message.Keyboard;
 
 const bot = new ViberBot({
 	authToken: '49e872d482e7d5f4-26150ff0836449f8-3f0f198008f8c8e6',
@@ -11,10 +20,26 @@ const bot = new ViberBot({
 	avatar: "https://dl-media.viber.com/1/share/2/long/vibes/icon/image/0x0/800b/b85aacbdc579999439e781f37f8bec50b1238229df591acc3996c50e97b5800b.jpg" // It is recommended to be 720x720, and no more than 100kb.
 });
 
-const minApiVersion = 6;
+const WELCOME_KEYBOARD = {
+	"Type": "keyboard",
+	"Revision": 1,
+	"Buttons": [
+		{
+			"Columns": 3,
+			"Rows": 2,
+			"BgColor": "#32CD32",
+			"Text": "Get Started"
+			"ActionType": "reply",
+			"ActionBody": "Hi"
+		}
+	]
+};
+const minApiVersion = "6";
+const welcomeKeyboard = new KeyboardMessage(WELCOME_KEYBOARD,"","","",minApiVersion);
 // Perfect! Now here's the key part:
-bot.onConversationStarted((userProfile, isSubscribed, context, onFinish) =>
-	onFinish(new TextMessage(`Hi, ${userProfile.name}! Nice to meet you.`)));
+bot.on(BotEvents.CONVERSATION_STARTED, (userProfile, isSubscribed, context, onFinish) =>
+	onFinish([new TextMessage(`Hi, ${userProfile.name}! Nice to meet you.`),
+			welcomeKeyboard]));
 
 
 bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
