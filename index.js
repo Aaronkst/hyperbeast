3c9ef44be4e06c1667f1e2ad6137b3e6f1d67085
Aaronkst <47111815+Aaronkst@users.noreply.github.com>
2019-09-10 20:03:01 +0630

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,12 @@ const WELCOME_KEYBOARD = {
 const minApiVersion = "6";
 const welcomeKeyboard = new KeyboardMessage(WELCOME_KEYBOARD,"GetStarted","","",minApiVersion);
 // Perfect! Now here's the key part:
-bot.on(BotEvents.CONVERSATION_STARTED, (userProfile, isSubscribed, context, onFinish) => console.log("onFinish", onFinish));
+bot.on(BotEvents.CONVERSATION_STARTED, (userProfile, isSubscribed, context, onFinish) => {
+	console.log("userProfile", userProfile);
+	console.log("isSubbed", isSubscribed);
+	console.log("context", context);
+	console.log("onFinish", onFinish);
+});
 
 
 bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
