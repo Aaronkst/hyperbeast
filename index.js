80b45682b177cf22860849e221f5c79a8ca63519
Aaronkst <47111815+Aaronkst@users.noreply.github.com>
2019-09-11 20:01:57 +0630

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ const userprofile = []
 // Perfect! Now here's the key part:
 bot.on(BotEvents.CONVERSATION_STARTED, (userProfile, isSubscribed, context, onFinish) => {
 	const uPF = userProfile.userProfile
-  console.log(uPF.name);
+  console.log(uPF);
 	userprofile.push(uPF);
 	bot.sendMessage(uPF,new TextMessage('Hi '+uPF.name+'! Welcome to Hyperbeast!',{
 	"Type": "keyboard",
