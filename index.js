765e4c8817b468ed447758812afc6b5a01e6e527
Aaronkst <47111815+Aaronkst@users.noreply.github.com>
2019-09-20 15:20:39 +0630

    Update index.js

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ bot.on(BotEvents.CONVERSATION_STARTED, (userProfile, isSubscribed, context, onFi
   }
   KEYBOARD_FRAME.Buttons = []
   KEYBOARD_FRAME.Buttons.push(button);
-	bot.sendMessage(userprofile[0],[new TextMessage('Hi!'+usernmae),(new KeyboardMessage(KEYBOARD_FRAME,"","","",minApiVersion))],["GetStarted"])
+	bot.sendMessage(userprofile[0],[new TextMessage('Hi! '+usernmae),(new KeyboardMessage(KEYBOARD_FRAME,"","","",minApiVersion))],["GetStarted"]).catch(function(error){console.log(error)});
 });
 
 
