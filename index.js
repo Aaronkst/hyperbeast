173b1e0e1b827ef7e60a519465228ae1a5becda4
Aaronkst <47111815+Aaronkst@users.noreply.github.com>
2019-09-20 15:30:11 +0630

    Update index.js

--- index.js
+++ index.js
@@ -42,21 +42,21 @@ bot.on(BotEvents.CONVERSATION_STARTED, (userProfile, isSubscribed, context, onFi
 	const uPF = userProfile.userProfile;
   const usernmae = uPF.name
 	userprofile.push(uPF);
-  let button = {
-    "Columns": 6,
-    "Rows": 1,
-    "BgColor": "#4b3695",
-    "Text": "<font color='#FFFFFF'>Get Started</font>",
-    "InputFieldState": "hidden",
-    "TextHAlign": "center",
-    "TextVAlign": "middle",
-    "ActionType": "reply",
-    "TextSize": "large",
-    "ActionBody": "Hi"
-  }
-  KEYBOARD_FRAME.Buttons = []
-  KEYBOARD_FRAME.Buttons.push(button);
-	bot.sendMessage(uPF,[new TextMessage('Hi! '+usernmae),(new KeyboardMessage(KEYBOARD_FRAME))]).catch(function(error){console.log(error)});
+	bot.sendMessage(uPF,[new TextMessage('Hi! '+usernmae),(new KeyboardMessage{
+      "Type":"keyboard",
+      "DefaultHeight":false,
+      "InputFieldState": "hidden",
+      "Buttons":[{"Columns": 6,
+          "Rows": 1,
+          "BgColor": "#4b3695",
+          "Text": "<font color='#FFFFFF'>Get Started</font>",
+          "InputFieldState": "hidden",
+          "TextHAlign": "center",
+          "TextVAlign": "middle",
+          "ActionType": "reply",
+          "TextSize": "large",
+          "ActionBody": "Hi"}]
+   })]).catch(function(error){console.log(error)});
 });
 
 
