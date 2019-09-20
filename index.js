f08923fa9dccf2b0796ad73e1dc844cda65be75c
Aaronkst <47111815+Aaronkst@users.noreply.github.com>
2019-09-20 15:38:41 +0630

    Update index.js

--- index.js
+++ index.js
@@ -146,9 +146,9 @@ bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
     RICHMEDIA_FRAME.Buttons.push(rmbutton4)
     RICHMEDIA_FRAME.Buttons.push(rmbutton5)
     RICHMEDIA_FRAME.Buttons.push(rmbutton6)
-		bot.sendMessage(userprofile[0],new TextMessage('These are the Hyperbeast Themed products!'))
-		bot.sendMessage(userprofile[0],(new RichMediaMessage(RICHMEDIA_FRAME,"","","",minApiVersion)),["CarouselChoice"])
-		bot.sendMessage(userprofile[0],(new KeyboardMessage(KEYBOARD_FRAME,"","","",minApiVersion)),["LocationShare"])
+		bot.sendMessage(userprofile[0],new TextMessage('These are the Hyperbeast Themed products!'),
+      (new RichMediaMessage(RICHMEDIA_FRAME,"","","",minApiVersion)),
+      (new KeyboardMessage(KEYBOARD_FRAME,"","","",minApiVersion)),["LocationShare"])
     
 	}
 	
